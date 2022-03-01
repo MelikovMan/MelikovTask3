9ab96cdb2aba043343c01aa503dc4ab929bb9850
MelikovMan <90448903+MelikovMan@users.noreply.github.com>
2022-03-01 15:08:32 +0300

    Add files via upload

--- backend/index.php
+++ backend/index.php
@@ -4,11 +4,14 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST'){
 	print_r('Does not accept non POST methods');
 }
 $errors = FALSE;
-if(empty($_POST['field-name-1']) || empty($_POST['field-email']) || empty($_POST['field-date']) || empty($_POST['bio-field'])){
+if(empty($_POST['field-name-1']) || isset($_POST['field-name-4']) || empty($_POST['field-email']) || empty($_POST['field-date']) || empty($_POST['bio-field'])){
 	print_r('Empty fields!');
 	exit();
 }
-print_r("Non null data.../n");
+print_r("Non null data... /n");
 print_r($_POST."/n");
-print_r($_POST['field-name-4'][0]);
+$superpowers = $_POST['field-name-4'];
+foreach ($superpowers as $val){
+	print_r($val);
+}
 exit();
