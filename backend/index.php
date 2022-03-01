a24bf1457f99e27eb208f81c766734ae791382d5
MelikovMan <90448903+MelikovMan@users.noreply.github.com>
2022-03-01 15:30:29 +0300

    Add files via upload

--- backend/index.php
+++ backend/index.php
@@ -9,13 +9,13 @@ if(empty($_POST['field-name-1']) || !isset($_POST['field-name-4']) || empty($_PO
 	exit();
 }
 print_r("Non null data... /n");
-&name = $_POST['field-name-1'];
-&email = $_POST['field-email'];
-&birth = $_POST['field-date'];
-&sex = $_POST['radio-group-1'];
-&limbs = intval(substr($_POST['field-group-2'],-1));
+$name = $_POST['field-name-1'];
+$email = $_POST['field-email'];
+$birth = $_POST['field-date'];
+$sex = $_POST['radio-group-1'];
+$limbs = intval(substr($_POST['field-group-2'],-1));
 $superpowers = $_POST['field-name-4'];
-&name = $_POST['bio-field'];
+$name = $_POST['bio-field'];
 print_r($birth);
 $user = 'u47551';
 $pass = '4166807';
