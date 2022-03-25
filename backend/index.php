0643433f24c63db0f07204fd1d1ddc04eea8b2b7
Kigryoszen <mrmalrojr@gmail.com>
2022-03-25 09:04:41 +0300

    eeeee

--- backend/index.php
+++ backend/index.php
@@ -37,7 +37,6 @@ if(!preg_match($dateregex,$birth)){
 	exit();
 }
 preg_match_all("/\d+/",$birth,$matches);
-print_r($matches);
 if (!checkdate($matches[0][1],$matches[0][2],$matches[0][0])){
 	print_r('Date does not exist');
 	exit();
